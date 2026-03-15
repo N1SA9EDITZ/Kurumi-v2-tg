7d4baf837d0924b6185480cfd9f1b975b06f77fc
✨NI S AN✨ <nisanerror404@gmail.com>
2026-03-15 10:26:10 +0600

    Update 4k.js

--- scripts/cmds/4k.js
+++ scripts/cmds/4k.js
@@ -2,7 +2,7 @@ const a = require('axios');
 const tinyurl = require('tinyurl');
 const baseApiUrl = async () => {
   const base = await a.get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+    `https://noobs-api-team-url.vercel.app/N1SA9/baseApiUrl.json`,
   );
   return base.data.api;
 };
