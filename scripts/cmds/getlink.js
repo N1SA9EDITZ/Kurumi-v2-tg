15cf33671ea0c2bd3be3c60bb164b4925993ba0b
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-08 17:14:26 +0600

    Update getlink.js

--- scripts/cmds/getlink.js
+++ scripts/cmds/getlink.js
@@ -2,7 +2,7 @@ const shortenURL = require("tinyurl").shorten;
 const { get } = require("axios");
 const baseApiUrl = async () => {
   const base = await get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+    `https://noobs-api-team-url.vercel.app/N1SA9/baseApiUrl.json`,
   );
   return base.data.api;
 };
