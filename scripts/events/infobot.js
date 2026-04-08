1cb9b490173569f85c948db51bae831973af361b
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-08 14:47:35 +0600

    Update infobot.js

--- scripts/events/infobot.js
+++ scripts/events/infobot.js
@@ -1,8 +1,16 @@
 module.exports = {
     onChat: async ({ bot, msg }) => {
         if (msg.new_chat_member && msg.new_chat_member.id === bot.id) {
-            const infoMessage = `Hello! I'm ${bot.username}, the bot. How can I assist you today?`;
-            bot.sendMessage(msg.chat.id, infoMessage);
+            const infoMessage = `Hello! I'm ${bot.username}, the bot 🤖\nClick Start button below 👇`;
+
+            bot.sendMessage(msg.chat.id, infoMessage, {
+                reply_markup: {
+                    keyboard: [
+                        [{ text: "/start" }]
+                    ],
+                    resize_keyboard: true
+                }
+            });
         }
     }
 };
